84acdf7ee70a6e434406c90495b52e35173d6cfc
nag167 <nagarjunareddy167@gmail.com>
2019-07-29 13:15:19 +0530

    initial commit

---
+++ calc.java
@@ -0,0 +1,27 @@
+package calculator1;
+
+public class calc {
+
+
+public int add(int x,int y){
+return x+y;
+
+}
+public int sub(int x,int y){
+return x+y;
+
+}
+public int multiply(int x,int y){
+return x*y;
+
+}
+public int divide(int x,int y){
+return x/y;
+
+}
+public static void main(String[] args) {
+	calc c =new calc();
+	System.out.println(c.add(3,4));
+}
+}
+
